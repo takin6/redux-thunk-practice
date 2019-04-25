88ee7fe70f7994795050ea2c464a891518db1227
井上貴之 <takayuki@mbp.local>
2019-04-26 08:42:45 +0900

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+// import ChannelsField from './ChannelsField';
+// import Button from '../containers/Button';
+// import Topnews from '../containers/TopNews';
+
+const App = () => (
+  <div>
+    <h1>Hello World</h1>
+  </div>
+)
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { render } from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import { logger } from 'redux-logger';
+// import reducer from './reducers';
+// import './css/styles.css';
+import App from './components/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(
+  reducer,
+  applyMiddleware(thunk, logger)
+)
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
